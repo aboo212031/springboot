6ba20916369ccb923bfc08f2eb353814c4d57ee5
Abubakr Rizan <74781555+aboo212031@users.noreply.github.com>
2025-05-08 18:11:06 +0000

    Controllers, Templates and Models

--- src/main/java/com/codewithmosh/store/entities/Address.java
+++ src/main/java/com/codewithmosh/store/entities/Address.java
@@ -33,4 +33,8 @@ public class Address {
     @JoinColumn(name = "user_id")
     @ToString.Exclude
     private User user;
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 }
